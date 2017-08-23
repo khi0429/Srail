15ad603bd5f7017fa1a1e8f42417c810688d5fcf
iibi0107 <iibi0107@iibi0107-PC>
2017-08-23 14:04:15 +0900

    do 확인

---
+++ Srail/src/main/java/com/common/controller/CalendarController.java
@@ -0,0 +1,17 @@
+package com.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class CalendarController {
+	
+	@RequestMapping("/calendar.do")
+	public String showIndex(Model m){
+		System.out.println("do실행");
+		return "mainpage/calendar";
+	}
+	
+}
